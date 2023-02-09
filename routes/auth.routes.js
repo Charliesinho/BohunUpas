3fc29d03865dbe4d1438a0827e6c9e91522545bf
Charlie Trestini <charlietrestini@hotmail.com>
2023-02-09 13:51:21 +0000

    signup

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,14 +1,31 @@
-
-// Hello I will be gone
-
 const express = require('express');
 const router = express.Router();
+const UserModel = require("../models/User.model")
+const bcrypt = require('bcryptjs');
 
 /* GET home page */
 router.get("/signup", (req, res, next) => {
-  res.render("auth/signup");
+  res.render("auth/signup", {errorMessage: ""});
 });
 
+router.post("/signup", async (req, res) => {
+    const body = {...req.body};
+
+    const salt = bcrypt.genSaltSync(10);
+    const passwordHash = bcrypt.hashSync(body.password, salt);
+
+    delete body.password;
+
+    body.passwordHash = passwordHash;
+
+    try  {
+     await UserModel.create(body);
+    }
+    catch (error) {
+     console.log(error);
+    }
+    res.redirect("/auth/signup")
+})
 
 module.exports = router;
->>>>>>> 36c51b8a4fd5ccc7c927538877f27eb615619981
+
