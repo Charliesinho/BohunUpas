d13d96ce6ef447867e4b4de91375a3419999af2d
Maik Schmidt <maikschmidt91@gmail.com>
2023-02-16 15:14:13 +0100

    Deployment test

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const app = require("./app");
 
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
 const PORT = process.env.PORT || 3000;
+const http = require('http');
+
+const server = http.createServer(server);
+server.listen(PORT);
 
 
 app.listen(PORT, () => {
